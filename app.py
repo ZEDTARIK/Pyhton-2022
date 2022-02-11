ca10c79db1ca5ccef1f22a8b94f906c0583bb84c
zouhair <ettarak.zouhair@gmail.com>
2022-02-11 15:55:32 +0100

    Surface et Peremtre

---
+++ app.py
@@ -0,0 +1,8 @@
+Larg = float(input("Saisir votre Largeur : "))
+Long = float(input("Saisir votre Longueur: "))
+
+S = Larg * Long
+P = (Larg + Long)*2
+
+print("La surface du Rectangle est : ", format(S, ".2f"))
+print("le péremètre du Rectangle est :", format(P, ".2f"))
