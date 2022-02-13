78f4d6528914f107374dc483a6d951554a38392d
zouhair <ettarak.zouhair@gmail.com>
2022-02-13 12:42:55 +0100

    Update fomat

--- Sphere.py
+++ Sphere.py
@@ -1,10 +1,7 @@
-from traceback import format_exception
-
-
 import math
 
 S = float(input("Saisir le rayon d'une sphere : "))
 PY = 3.14
-V = (4* math.pi * (S**3) )/3
+V = (4 * math.pi * (S ** 3)) / 3
 
 print("Le volume d'une sp3hére est de : ", format(V, ".2f"))
